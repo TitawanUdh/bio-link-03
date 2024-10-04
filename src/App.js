a53330109db9fe8933554fb2e4bb8b3ce226f427
Titawan <titawan.udh@gmail.com>
2024-10-04 16:28:32 +0700

    'update'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Image } from "react-bootstrap";
+import imgBg from "../src/images/bg-02.jpg";
+import "./App.css";
+import imgPerson from '../src/images/person-02.jpg'
 function App() {
+  const data = [
+    {
+      title: "Tiktok",
+      url: "www.tiktok.com",
+    },
+    {
+      title: "Facebook",
+      url: "www.facebook.com",
+    },
+    {
+      title: "Instagrams",
+      url: "www.instagrams.com",
+    },
+    {
+      title: "Line OA",
+      url: "www.line.com",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div
+ style={{
+  backgroundImage: `url(${imgBg})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}}
+ >
+  <Image
+  src={imgPerson}
+  className="img-personal mb-2"/>
+  <h2>Marien Lasonce</h2>
+  {data.map((d, i)=> (
+    <a href={d.url}  className="text-link">
+      <div className="social-box">{d.title}</div>
+    </a>
+  ))}
+
+ </div>
   );
 }
 
